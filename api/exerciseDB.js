031681b772931a9682b019c95ff7097e39bca72a
Syed Maaz Ali <74668200+syedmaaz9905@users.noreply.github.com>
2024-04-25 19:38:56 +0500

    first commit

---
+++ api/exerciseDB.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { rapidApiKey } from "../constants";
+
+const baseUrl = 'https://exercisedb.p.rapidapi.com';
+
+const apiCall = async (url, params) => {
+    try {
+        const options = {
+            method: 'GET',
+            url,
+            params,
+            headers: {
+                'X-RapidAPI-Key': rapidApiKey,
+                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+            }
+        };
+
+        const response = await axios.request(options);
+        return response.data;
+
+    } catch (err) {
+        console.log('Error: ', err.message);
+    }
+}
+
+export const fetchExercisesByBodyPart = async (bodyPart) => {
+    let data = await apiCall(baseUrl + `/exercises/bodyPart/${bodyPart}`);
+    return data;
+}
